66ca2b5f6a90d19dffae66df52ab844e32b8adf5
yajie <puu882@gmail.com>
2024-07-06 10:11:14 +0800

    Fix: modify the code of the fronend to make the CICD pipeline run fluently

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
-// File: src/App.jsx
+
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import React, { useState } from 'react';
 import './App.css';
 import AuthPage from './AuthPage';
 import MapPage from './MapPage';
 import Dashboard from './Dashboard';
-import Test from './test';
-import logo from './logo.png'; // 將您的logo圖片路徑修改為實際路徑
-// import 'bootstrap/dist/css/bootstrap.min.css';
+
+import logo from './logo.png'; 
+
 
 function App() {
  
@@ -28,13 +28,6 @@ function App() {
               <li>
                 <Link to="/dashboard">路線紀錄</Link>
               </li>
-              {/* <li>
-                <Link to="/map">登山路線規劃</Link>
-              </li>
-        
-              <li>
-                <Link to="/test">測試網頁</Link>
-              </li> */}
             </ul>
           </nav>
         </header>
