b38c92125c1f7dc57026975cc79c313b6fd8314d
yajie <puu882@gmail.com>
2024-07-10 16:18:49 +0800

    Fix: socket.io problem

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -21,7 +21,7 @@ const Dashboard = () => {
 
     const fetchRoutes = async () => {
       try {
-        const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/route/user-paths/${id}`);
+        const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/route/user-paths/1`);
         setRoutes(response.data);
         console.log('route',response.data)
       } catch (error) {
